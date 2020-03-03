0397e9f9e95805ede47b8b7a07115c4cef720f89
Odong Gilbert <47414230+gilbert-256@users.noreply.github.com>
2020-03-03 09:36:41 +0300

    nodejs express

---
+++ index.js
@@ -0,0 +1,8 @@
+// require express dependency to include the express functionality
+const express = require('express');
+// creates express server ->to send/output data
+const server=express();
+
+server.listen(3000,function(){
+    console.log('Listening on 3000')
+})
